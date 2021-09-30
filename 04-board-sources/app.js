79a1bb1937959526af1e7851537f146f1e79cb71
KORODEN <korogodin.denis@mail.ru>
2021-09-30 14:08:34 +0700

    next

--- 04-board-sources/app.js
+++ 04-board-sources/app.js
@@ -1,25 +1,69 @@
 const board = document.querySelector('#board')
-const SQUARES_NUMBER = 500
 const colors = ['#fe5e31', '#fe9001', '#fed3a5', '#72b8c9', '#5b7c7d']
 
-for(let i = 0; i < SQUARES_NUMBER; i++) {
-    const square = document.createElement('div')
-    square.classList.add('square')
+let isStarted = false
 
-    square.addEventListener('mouseover', () => setColor(square))
+const startBtn = document.querySelector('#start')
+const clearBtn = document.querySelector('#clear')
+const input = document.querySelector('#number')
 
-    square.addEventListener('mouseleave', () => removeColor(square))
+startBtn.addEventListener('click', start)
+clearBtn.addEventListener('click', clear)
 
-    board.append(square)
+input.addEventListener('change', () => {
+    isStarted = false
+})
+
+function start() {
+    if(!isStarted) {
+        board.innerHTML = nul
+        let SQUARES_NUMBER = input.value
+
+        if(SQUARES_NUMBER > 700){
+            SQUARES_NUMBER = 700
+            input.value = SQUARES_NUMBER
+            board.style.boxShadow = '0 0 10px #fe5e31'
+            setTimeout(() => {
+                board.style.boxShadow = 'none'
+            }, 1300)
+        } else if (SQUARES_NUMBER < 100) {
+            SQUARES_NUMBER = 100
+            input.value = SQUARES_NUMBER
+            board.style.boxShadow = '0 0 10px #fe5e31'
+            setTimeout(() => {
+                board.style.boxShadow = 'none'
+            }, 1300)
+        }
+
+        for(let i = 0; i < SQUARES_NUMBER; i++) {
+            const square = document.createElement('div')
+            square.classList.add('square')
+        
+            square.addEventListener('mouseover', setColor)
+            square.addEventListener('mouseleave', removeColor)
+        
+            board.append(square)
+        }
+    
+        isStarted = true
+    }
 }
 
-function setColor(element) {
+function clear() {
+    board.innerHTML = null
+    input.value = 100
+    isStarted = false
+}
+
+function setColor(event) {
+    const element = event.target
     const color = getRandomColor()
     element.style.backgroundColor = color
     element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
-function removeColor(element) {
+function removeColor(event) {
+    const element = event.target
     element.style.backgroundColor = '#1d1d1d'
     element.style.boxShadow = '0 0 2px #000'
 }
